b4c0c5286f4a32b2f904d1fcedfcc94b7d117a62
Ayush Khakharia <khakharia.ayush20@gmail.com>
2021-07-12 21:46:43 +0530

    Final Commit

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,7 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { RotateLoader } from 'react-spinners'
+import { css } from '@emotion/react'
+import ABCJS from 'abcjs'
 
 const HomeScreen = () => {
     const [input, setInput] = useState('')
+    const [abcText, setAbcText] = useState('')
+    const [loader, setLoader] = useState(false)
+    const override = css`
+        margin: 0 auto;
+        border-color: black;
+    `
+    
+    const showSheetMusic = () => {
+        setLoader(true)
+        fetch('/api')
+        .then(response => response.json())
+        .then(data => {
+            setAbcText(data.message)
+            ABCJS.renderAbc('target', abcText)
+            setLoader(false)
+        })
+        // ABCJS.renderAbc('target', abcText)
+        // console.log('after rendering')
+    }
 
     return(
         <div className='homescreen'>
@@ -12,13 +34,23 @@ const HomeScreen = () => {
                 <h3>Wanna listen to music,Enjoy your own music</h3>
                 
                 <div className='homescreen_input' align='center'>
-                    <input
+                    {/* <input
                      placeholder=' Enter your text here '
                      onChange={(e) => setInput(e.target.value)}
                      value={input} 
-                    />
-                    <button className='homescreen_button' type='submit'> Generate</button>
-                    <p> {input} </p>
+                    /> */}
+                    {loader ? (
+                        <RotateLoader
+                            className='homescreen_loader' 
+                            loading
+                            size={50}
+                            color= '#000'
+                        />) : (
+                            <button onClick={() => showSheetMusic()} className='homescreen_button' type='submit'> Generate</button>
+                        )}
+                    
+                    
+                    <div align='center' id='target'></div>
                 </div>
                 
             </div>
