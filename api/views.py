23d87d7b6d4f1e0c5b549f4ede0d589b508e8edb
Ayush Khakharia <khakharia.ayush20@gmail.com>
2021-07-14 19:58:19 +0530

    Final Commit

--- api/views.py
+++ api/views.py
@@ -3,11 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .util import sample
-import os
 
-# Create your views here.
-# def index(request):
-#     return HttpResponse('hello world')
 class GenerateMusic(APIView):
     def get(self, request, format=None):
         result = sample(100 , '' , 2048)
--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,44 +1,37 @@
 import React, {useState, useEffect} from 'react'
 import { RotateLoader } from 'react-spinners'
-import { css } from '@emotion/react'
 import ABCJS from 'abcjs'
 
 const HomeScreen = () => {
-    const [input, setInput] = useState('')
     const [abcText, setAbcText] = useState('')
     const [loader, setLoader] = useState(false)
-    const override = css`
-        margin: 0 auto;
-        border-color: black;
-    `
-    
+    const [show, setShow] = useState(false)    
+
+    useEffect(() => {
+        if(show)
+            ABCJS.renderAbc('target', abcText)           
+    },[show])
+
     const showSheetMusic = () => {
         setLoader(true)
         fetch('/api')
         .then(response => response.json())
         .then(data => {
             setAbcText(data.message)
-            ABCJS.renderAbc('target', abcText)
+            setShow(true)
             setLoader(false)
+            setShow(false)
         })
-        // ABCJS.renderAbc('target', abcText)
-        // console.log('after rendering')
     }
 
     return(
         <div className='homescreen'>
-            <div className='homescreen_image' />
 
             <div className='homescreen_body' align='center'>
                 <h1>Welcome to Musica</h1>
                 <h3>Wanna listen to music,Enjoy your own music</h3>
                 
                 <div className='homescreen_input' align='center'>
-                    {/* <input
-                     placeholder=' Enter your text here '
-                     onChange={(e) => setInput(e.target.value)}
-                     value={input} 
-                    /> */}
                     {loader ? (
                         <RotateLoader
                             className='homescreen_loader' 
